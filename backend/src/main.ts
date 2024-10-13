d9b52d70c83f4f85798cb2b30addc3fb382d8ea2
martinezmauri <maurimartine01@gmail.com>
2024-10-12 22:44:43 -0300

    agrego user

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('App');
   const app = await NestFactory.create(AppModule);
+  logger.log('server listening on port 3000');
   await app.listen(3000);
 }
 bootstrap();
